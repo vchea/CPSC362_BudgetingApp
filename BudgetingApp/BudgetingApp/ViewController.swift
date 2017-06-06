2dd070e1ca486fdac2dfe582282726dae3078199
jeffreyguerra <jeffreyguerra@csu.fullerton.edu>
2017-06-06 11:14:00 -0700

    Updated UI - Load Screen
    Bug when clicking "Sign up" link

--- BudgetingApp/BudgetingApp/ViewController.swift
+++ BudgetingApp/BudgetingApp/ViewController.swift
@@ -6,21 +6,26 @@
 //  Copyright © 2017 CPSC362_FinanciallyResponsible. All rights reserved.
 //
 
-
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+   
+    @IBOutlet weak var signupBTN: UIButton!
+   
+    @IBOutlet weak var signupbutton: UIButton!
+    @IBOutlet weak var time: UIImageView!
+    @IBOutlet weak var passwordtextfield: UITextField!
+    @IBOutlet weak var passwordlabel: UILabel!
     @IBOutlet weak var financialbuddylabel: UILabel!
     
     @IBOutlet weak var budgetlabel: UILabel!
     
     @IBOutlet weak var budgetinput: UITextField!
     
-    @IBOutlet weak var submitbudget: UIButton!
-    
-    
+    @IBOutlet weak var submitlogin: UIButton!
     
     
     override func viewDidLoad() {
@@ -34,5 +39,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func signuppressed(_ sender: Any) {
+        signupbutton.isHidden = true
+        time!.isHidden = false
+        
+
+    }
 }
 
